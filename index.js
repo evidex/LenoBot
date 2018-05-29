23deff3487eca406238bb86f52e51ad6af811b6c
invisusira <atraira@gmail.com>
2018-05-28 21:15:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-import discordIRC from 'discord-irc';
-import config from 'config.json';
-discordIRC(config);
+// discord-irc --config config.json
 
+const discordIRC = require('discord-irc');
+const config = require('config.json');
 const commando = require('discord.js-commando');
 const bot = new commando.Client();
+
+discordIRC(config);
 
 bot.registry.registerGroup('emoji', 'Emoji');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands")
 
-bot.login(process.env.BOT_TOKEN);
+bot.login(process.env.NDUwNDc3NTk1NzE3NTMzNjk2.De0B0Q.m-v9ptlenxKSKQxxZSJqWsekEVQ);
