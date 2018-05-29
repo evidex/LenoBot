eeee5f7399166e95b7a4c73d667579a9badc99ca
invisusira <atraira@gmail.com>
2018-05-28 20:25:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ bot.registry.registerGroup('emoji', 'Emoji');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands")
 
+import discordIRC from 'discord-irc';
+import config from 'config.json';
+discordIRC(config);
+
 bot.login(process.env.BOT_TOKEN);
